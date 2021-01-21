3aa1c5560a6a551dd5597c6cadb8cbb137fb2aba
AsureDay <73357830+AsureDay@users.noreply.github.com>
2021-01-21 23:31:53 +0700

    Create testLib.py

---
+++ DFT/testLib.py
@@ -0,0 +1,17 @@
+import cv2 as cv
+import numpy as np 
+from matplotlib import pyplot as plt
+
+img = cv.imread("georges\\170d5b93d80d247be60f22ca1216bef7.jpg",0)
+
+
+f = np.fft.fft2(img)
+fshift = np.fft.fftshift(f)
+magnitude_spectrum = 20*np.log(np.abs(fshift))
+#main things 
+
+plt.subplot(121),plt.imshow(img, cmap = 'gray')
+plt.title('Input Image'), plt.xticks([]), plt.yticks([])
+plt.subplot(122),plt.imshow(magnitude_spectrum, cmap = 'gray')
+plt.title('Magnitude Spectrum'), plt.xticks([]), plt.yticks([])
+plt.show()
